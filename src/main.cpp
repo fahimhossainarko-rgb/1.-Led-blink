9bd3fe4d59bd3feabc3b7d203214337d6049cc06
1.Led Blink <fahim.hossain.arko@gmaail.com>
2025-11-11 15:48:57 +0600

    code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH);
+  delay(2000);
+  digitalWrite(13,LOW);
+  delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
